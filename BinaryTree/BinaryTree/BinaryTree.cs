363cef0f36304670761fd2089cc08db290dafaf1
Cristian Canedo <cristiancanedo@outlook.com>
2017-07-08 23:54:32 -0400

    Add files via upload

---
+++ BinaryTree/BinaryTree/BinaryTree.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BinaryTree
+{
+    public class BinaryTree
+    {
+        public Node Root { get; set; }
+
+        public BinaryTree()
+        {
+            Root = null;
+        }
+        public BinaryTree(int value)
+        {
+            Root = new Node(value);
+        }
+
+        public bool InsertValue(int value)
+        {
+            Node toInsert = new Node(value);
+            Node current = Root;
+            bool inserted = false;
+
+            while (!inserted)
+            {
+                if (value < current.Data)
+                {
+                    if (current.Left == null)
+                    {
+                        current.Left = toInsert;
+                        return inserted = true;
+                    }
+                    else
+                    {
+                        current = current.Left;
+                    }
+                }
+                if (value >= current.Data)
+                {
+                    if (current.Right == null)
+                    {
+                        current.Right = toInsert;
+                        return inserted = true;
+                    }
+                    else
+                    {
+                        current = current.Right;
+                    }
+                }
+            }
+            return inserted;
+        }
+
+        
+        public void PrintTree(Node node)
+        {
+            if (node == null)
+            {
+                node = Root;
+            }
+
+            if (node.Left != null)
+            {
+                PrintTree(node.Left);
+                Console.Write(" " + node.Data);
+            }
+            else
+            {
+                Console.Write(" " + node.Data);
+            }
+
+            if (node.Right != null)
+            {
+                PrintTree(node.Right);
+            }
+
+        }
+        
+
+    }
+}
---
+++ BinaryTree/BinaryTree/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BinaryTree
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BinaryTree btree = new BinaryTree(50);
+            Console.WriteLine(btree.InsertValue(51));
+            Console.WriteLine(btree.InsertValue(49));
+            Console.WriteLine(btree.InsertValue(52));
+            Console.WriteLine(btree.InsertValue(48));
+            Console.WriteLine(btree.InsertValue(34));
+            Console.WriteLine(btree.InsertValue(22));
+            Console.WriteLine(btree.InsertValue(18));
+            Console.WriteLine(btree.InsertValue(10));
+
+            btree.PrintTree(btree.Root);
+
+            Console.WriteLine();
+
+        }
+    }
+}
